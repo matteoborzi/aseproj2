e760d463e8e8ebf5ae612b07578031cf13015d1e
MacSpowman <62106872+MacSpowman@users.noreply.github.com>
2021-01-17 21:36:08 +0100

    src: new bitmaps for player and obstacles

--- sample.c
+++ sample.c
@@ -26,6 +26,7 @@
 #include "TouchPanel/TouchPanel.h"
 #include "timer/timer.h"
 #include "RIT/RIT.h"
+#include "labyrinth/labyrinth.h"
 
 #ifdef SIMULATOR
 extern uint8_t ScaleFlag; // <- ScaleFlag needs to visible in order for the emulator to find the symbol (can be placed also inside system_LPC17xx.h but since it is RO, it needs more work)
@@ -45,9 +46,10 @@ int main(void)
 	LCD_Clear(Black);
 	GUI_Text(16,4, (uint8_t *) "Blind Labyrinth", Yellow, Black, 2);
 	
-	PrintMap(240,208,GREEN);
-	Print_Player(7* 16, 40+(7 * 16), 0, 0, Blue, GREEN);
-
+	PrintMap(240,208,White);
+	Print_Player(7,7,SOUTH,0);
+	//Print_Wall(11,7);
+	
 	enable_RIT();
 	
 	//init_timer(0, 0x1312D0 ); 						/* 50ms * 25MHz = 1.25*10^6 = 0x1312D0 */
